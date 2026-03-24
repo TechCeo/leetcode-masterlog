449b32a78cabc4c49c91ab970548fbb6029fb731
TechCeo <adamuyusuf3g@gmail.com>
2026-03-23 23:44:36 -0400

    Update 36_valid_sudoku.py
    Bitmask Optimized

--- medium/36_valid_sudoku.py
+++ medium/36_valid_sudoku.py
@@ -105,6 +105,31 @@ class Solution:
                 squareSet[(i // 3, j // 3)].add(val)
 
         return True
+    
+    
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [0] * 9
+        cols = [0] * 9
+        squares = [0] * 9
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+
+                val = int(board[r][c]) - 1
+                if (1 << val) & rows[r]:
+                    return False
+                if (1 << val) & cols[c]:
+                    return False
+                if (1 << val) & squares[(r // 3) * 3 + (c // 3)]:
+                    return False
+
+                rows[r] |= (1 << val)
+                cols[c] |= (1 << val)
+                squares[(r // 3) * 3 + (c // 3)] |= (1 << val)
+
+        return True
 
 
 # Test Cases
@@ -140,3 +165,6 @@ if __name__ == "__main__":
 
     test(sol.isValidSudoku_optimized, valid_board, True, "Optimized - Valid")
     test(sol.isValidSudoku_optimized, invalid_board, False, "Optimized - Invalid")
+    
+    test(sol.isValidSudoku, valid_board, True, "Bitmask Optimized - Valid")
+    test(sol.isValidSudoku, invalid_board, False, "Bitmask Optimized - Invalid")
